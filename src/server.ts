15621cfe15ff0c72996d6e68a8eac7a1010705e6
Euclides Baltazar <167758418+TheRealCocky@users.noreply.github.com>
2026-02-23 17:11:07 +0100

    Add keep-alive endpoint and fix port for Render

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,15 @@ app.use('/api/candidates',candidateRoutes);
 app.get('/', (req: Request, res: Response) => {
     res.send(`API de Candidatos do Grupo Zeno rodando`);
 });
+// para manter o render alive
+app.get('/api/keep-alive', (req: Request, res: Response) => {
+    console.log("Bot bateu aqui para manter o servidor acordado ");
+    res.status(200).json({
+        status: "online",
+        message: "Grupo Zeno API ativa",
+        timestamp: new Date().toISOString()
+    });
+});
 
 app.listen(process.env.PORT, ()=> {
     console.log(`Servidor rodando na porta http://localhost:${port}`);
